3384b5237f6439dbd8d486a9380c07baf752f49c
SebastianLlanes <sebastianllanessaavedra@gmail.com>
2023-03-22 15:29:55 -0300

    [Comentarios explicativos] 2/4 componentes so far

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -1,26 +1,38 @@
+// Importar React y useState desde el paquete "react"
 import React, { useState } from "react";
 
+// Declaración de un componente llamado ReservationForm que recibe una función llamada addReservation como prop
 const ReservationForm = ({ addReservation }) => {
+
+  // Utilizar el hook useState para crear un estado para cada campo de entrada del formulario
   const [name, setName] = useState("");
   const [guests, setGuests] = useState("");
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
   const [phone, setPhone] = useState("");
  
+  // Declarar una función que se ejecutará cuando se envíe el formulario
   const handleSubmit = (e) => {
+    // Prevenir que la página se recargue al enviar el formulario
     e.preventDefault();
+    // Crear un objeto de reserva con los valores actuales de cada estado y un id generado
     const reservation = { id: generateId(), name, guests, date, time, phone };
+    // Llamar a la función addReservation pasando la reserva como argumento
     addReservation(reservation);
+    // Reiniciar cada estado a su valor inicial
     setName("");
     setGuests("");
     setDate("");
     setTime("");
     setPhone("");
   };
+  
+  // Declarar una función que genera un id único basado en la fecha actual
   const generateId = () => {
     return Date.now().toString();
   };
 
+  // Renderizar el formulario con cada campo de entrada y un botón para enviar el formulario
   return (
     <form onSubmit={handleSubmit} className='form-container' >
       <input
@@ -59,4 +71,8 @@ const ReservationForm = ({ addReservation }) => {
   );
 };
 
+// Exportar el componente ReservationForm como default
 export default ReservationForm;
+
+
+// En resumen, este componente React es un formulario de reserva que utiliza el hook useState para mantener el estado de cada campo de entrada. Cuando el formulario se envía, se crea un objeto de reserva con los valores actuales de cada estado y se llama a una función pasada como prop para agregar la reserva a una lista. Después de agregar la reserva, cada estado se reinicia a su valor inicial. Además, el componente tiene una función que genera un id único basado en la fecha actual.
--- src/components/ReservationList.js
+++ src/components/ReservationList.js
@@ -1,25 +1,34 @@
-  import React from "react";
-  import ReservationCard from "./ReservationCard";
+import React from "react";
+import ReservationCard from "./ReservationCard";
   
+  // Esta es la definición del componente ReservationList
+  // que recibe dos props: reservations y cancelReservation
   const ReservationList = ({ reservations, cancelReservation }) => {
-
-
-   const sortedReservations = reservations.sort((a, b) => {
+  
+    // Ordenamos las reservas por fecha y hora utilizando
+    // la función sort() y creando objetos Date para comparar
+    const sortedReservations = reservations.sort((a, b) => {
       const dateA = new Date(`${a.date}T${a.time}`);
       const dateB = new Date(`${b.date}T${b.time}`);
       return dateA - dateB;
     });
+  
+    // Este es el JSX que será renderizado por el componente
+    return (
+      <div className="reservation-list">
+        {sortedReservations.map((reservation) => (
+          <ReservationCard
+            key={reservation.id}
+            reservation={reservation}
+            cancelReservation={cancelReservation}
+          />
+        ))}
+      </div>
+    );
+  };
+  
+  // Exportamos el componente para que pueda ser utilizado en otros archivos
+  export default ReservationList;
+  
 
-     return <div className="reservation-list">
-      {sortedReservations.map((reservation) => (
-         <ReservationCard
-         key={reservation.id}
-         reservation={reservation}
-         cancelReservation={cancelReservation}
-       />
-      ))}
-      </div>;
-   };
-   export default ReservationList;
-
-
+  // En resumen, este componente recibe una lista de reservas (reservations) y una función para cancelar reservas (cancelReservation), y se encarga de ordenarlas por fecha y hora antes de renderizarlas. Luego, para cada reserva en la lista, renderiza un componente ReservationCard con las propiedades reservation y cancelReservation.
