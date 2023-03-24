da1ef2f203999be11338c4df2fc08618ad5ed5f0
SebastianLlanes <sebastianllanessaavedra@gmail.com>
2023-03-24 09:54:17 -0300

    [Reservas Button] Save and Load Buttons

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import TableStock from "./components/TableStock";
 import "./App.css";
 
 const App = () => {
-  const [reservations, setReservations] = useState([]);
-  const [tables, setTables] = useState({
+  // Define estados iniciales
+  const [reservations, setReservations] = useState([]); // Lista de reservas
+  const [tables, setTables] = useState({ // Stock de mesas
     table2: 10,
     table4: 8,
     table6: 6,
     specialTable: 2,
   });
 
+  // Función para actualizar el stock de mesas
   const handleTablesUpdate = (updatedTables) => {
     setTables(updatedTables);
   };
 
+  // Efecto para cargar las reservas guardadas en el almacenamiento local al cargar la página
   useEffect(() => {
     const storedReservations = localStorage.getItem("reservations");
     if (storedReservations) {
@@ -24,11 +27,27 @@ const App = () => {
     }
   }, []);
 
+  // Efecto para guardar las reservas en el almacenamiento local al actualizar las reservas
   useEffect(() => {
     localStorage.setItem("reservations", JSON.stringify(reservations));
   }, [reservations]);
 
+  // Función para guardar las reservas en el almacenamiento local y mostrar un mensaje de éxito
+  const handleSaveReservations = () => {
+    localStorage.setItem("reservationsBackup", JSON.stringify(reservations));
+    alert("Reservas guardadas correctamente");
+  };
 
+    // Función para cargar las reservas guardadas del almacenamiento local y mostrar un mensaje de éxito o error
+  const handleLoadReservations = () => {
+    const storedReservations = localStorage.getItem("reservationsBackup");
+    if (storedReservations) {
+      setReservations(JSON.parse(storedReservations));
+      alert("Reservas cargadas correctamente");
+    } else {
+      alert("No hay reservas guardadas");
+    }
+  };
   
   const [tableAvailability, setTableAvailability] = useState({
     table2: true,
@@ -37,6 +56,7 @@ const App = () => {
     specialTable: true,
   });
 
+  // Función para agregar una reserva
   const addReservation = (reservation) => {
     if (
       !reservation.guests ||
@@ -53,7 +73,7 @@ const App = () => {
           : reservation.guests <= 6
           ? "table6"
           : "specialTable";
-      if (tableAvailability[tableType]) {
+      if (tableAvailability[tableType]) {  // Comprobar si hay mesas disponibles
         setReservations([...reservations, reservation]);
         updateTableStock(reservation, true);
       } else {
@@ -62,6 +82,7 @@ const App = () => {
     }
   };
 
+  // Función para cancelar una reserva
   const cancelReservation = (id) => {
     const filteredReservations = reservations.filter(
       (reservation) => reservation.id !== id
@@ -74,6 +95,8 @@ const App = () => {
     clearInterval(reservationToCancel.timerId);
   };
 
+
+   // Función para actualizar el stock de mesas
   const updateTableStock = (reservation, isReservation) => {
     let tableType;
     if (reservation.guests <= 2) {
@@ -97,9 +120,13 @@ const App = () => {
  
   return (
     <div className="app">
-      <h1>Reservas Clora</h1>
+      <h1>CLORA</h1>
       <div className="container" key="reservation-container">
-        <h2>Ingreso de Reserva</h2>
+        <div className="app-container-container">
+          <h2>Ingreso de Reserva</h2>
+            <button className="save-button" onClick={handleSaveReservations}>Guardar Reservas</button>
+            <button className="save-button" onClick={handleLoadReservations}>Cargar Reservas</button>
+        </div>
         <ReservationForm addReservation={addReservation} />
         <TableStock  tables={tables} onTablesUpdate={handleTablesUpdate} />
       </div>
